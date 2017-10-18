f38679fe773dd7264a13b55b92e8a053ad533b4a
basriumar12 <basriumar12@gmail.com>
2017-10-19 06:45:08 +0700

    v1

---
+++ app/src/main/java/com/echessa/videotube/Config.java
@@ -0,0 +1,14 @@
+package com.echessa.videotube;
+
+/**
+ * Created by echessa on 1/13/17.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyASxs8XEaG-cjcNu2Ubr75wb-5AxkPz27g";
+
+}
